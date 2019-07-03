b0ccaea32f1d5b05952672914e91e63b7943e119
Yuri Alves <yurialves2@gmail.com>
2019-07-03 19:57:23 -0300

    first ModelAndView - injecting data

--- src/main/java/com/yuri/twgerenciadortarefas/controllers/HomeController.java
+++ src/main/java/com/yuri/twgerenciadortarefas/controllers/HomeController.java
@@ -1,13 +1,19 @@
 package com.yuri.twgerenciadortarefas.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home/home"; 
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("home/home");
+        mv.addObject("mensagem", "Mensagem do controller");
+
+        return mv;
     }
 }
