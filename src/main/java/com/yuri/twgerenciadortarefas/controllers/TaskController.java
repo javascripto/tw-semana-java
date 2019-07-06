7f130deb55f49d73a12d6740471511b7a0f1e0e9
Yuri Alves <yurialves2@gmail.com>
2019-07-05 22:27:58 -0300

    listing tasks and using template fragments

--- src/main/java/com/yuri/twgerenciadortarefas/controllers/TaskController.java
+++ src/main/java/com/yuri/twgerenciadortarefas/controllers/TaskController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yuri.twgerenciadortarefas.repositories.TaskRepository;
+
 
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     @GetMapping("/list")
     public ModelAndView list() {
-        ModelAndView mv = new ModelAndView("home/home");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("tasks/list");
+        mv.addObject("tasks", taskRepository.findAll());
         return mv;
     }
 }
